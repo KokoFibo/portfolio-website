4e89f1e2720a76ccda7c77b01f09c54ce045f552
KokoFibo <kokonacci@gmail.com>
2022-09-30 14:05:07 +0700

    first commit

--- resources/js/components/pages/home/index.vue
+++ resources/js/components/pages/home/index.vue
@@ -1,8 +1,6 @@
 <template>
-<h1>Welcome to homepage website</h1>
-<div>
-    <router-link to="/admin/home">Admin Dashboard</router-link>
-</div>
-        
-   
+    <h1>Welcome to homepage website</h1>
+    <div>
+        <router-link to="/admin/home">Admin Dashboard ok</router-link>
+    </div>
 </template>
